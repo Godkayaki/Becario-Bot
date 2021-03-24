141495d8e4924bbe10bea2d79175dfed1f860374
Godkayaki <daniplcm@gmail.com>
2021-03-24 20:03:05 +0100

    fixes

--- src/main.py
+++ src/main.py
@@ -18,4 +18,4 @@ async def on_message(message):
     if message.content.startswith('-reqpool'):
         await message.channel.send('Generating pool...')
 
-client.run('ODI0MzQyOTI0NzA3NDk1OTM2.YFt_LA.HCh3jR9wvMNXh_XbhU2NVryypXg')
+client.run('')
